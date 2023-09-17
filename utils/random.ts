a91b590364d54cf3be6c5ce316784e4eeaffffa8
bao <quocbaobk.ce@gmail.com>
2023-09-17 18:17:31 +0700

    fix question card

--- utils/random.ts
+++ utils/random.ts
@@ -1,18 +1,18 @@
 import { faker } from '@faker-js/faker';
+import { shuffleArray } from './array';
 
-export function getRandomWords(origin: string, number: number = 3) {
+export function getRandomWords(origin: string, limit: number = 3) {
   const expectedLength = origin.length > 1 ? origin.length : 2;
-  const randomWords: string[] = [];
-  for (let i = 0; i < number; i++) {
-    let randomWord: string;
-    do {
-      randomWord = faker.word.sample({
-        strategy: 'any-length',
-        length: { min: expectedLength, max: expectedLength + 3 },
-      });
-    } while (randomWord === origin && !randomWords.includes(randomWord));
-    randomWords.push(randomWord);
+  const randomWords: string[] = [origin];
+
+  while (randomWords.length < limit + 1) {
+    const randomWord = faker.word.sample({
+      strategy: 'any-length',
+      length: { min: expectedLength, max: expectedLength + 3 },
+    });
+
+    if (randomWord !== origin) randomWords.push(randomWord);
   }
 
-  return randomWords;
+  return shuffleArray<string>(randomWords);
 }
